8d378010d8d191104c2ef7311f77d86f1f544cf4
Leandro Leonardo Passarelli <llpassarelli@gmail.com>
2017-01-21 00:07:20 -0200

    Versão 2 - Detector por trigger de média
    Rescrevi o código, mais simples, robusto e bonito.
    Removido trigger por deteção de retângulos.
    Adicionado trigger por média de tom/cor para acionar o decodificador.
    Interface para detecção de média.
    Marcação da posição do código via libdmtx.

--- dmtxscann.py
+++ dmtxscann.py
@@ -4,7 +4,7 @@
 Python Data Matrix Scanner
 
 <llpassarelli@gmail.com>
-    
+
 use opencv and libdmtx
 
    q or ESC - exit
@@ -13,60 +13,69 @@ use opencv and libdmtx
 
 import cv2
 import numpy as np
-import sys
 from PIL import Image
 import os
+import sys
 import datetime
 import time
-
 from pydmtx import DataMatrix
 
-def angle_cos(p0, p1, p2):
-    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
-    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
-
-def find_squares(img0):
-    img = cv2.GaussianBlur(img0, (5, 5), 0)
-    squares = []
-    for gray in cv2.split(img):
-        for thrs in xrange(0, 255, 26):
-            if thrs == 0:
-                bin = cv2.Canny(gray, 0, 50, apertureSize=5)
-                bin = cv2.dilate(bin, None)
-            else:
-                retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
-            _, contours, hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            for cnt in contours:
-                cnt_len = cv2.arcLength(cnt, True)
-                cnt = cv2.approxPolyDP(cnt, 0.1*cnt_len, True)
-                if len(cnt) == 4 and cv2.contourArea(cnt) > 2000 and cv2.contourArea(cnt) < 30000 and cv2.isContourConvex(cnt):
-                    x,y,w,h = cv2.boundingRect(cnt)
-                    cnt = cnt.reshape(-1, 2)
-                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
-                    # print max_cos, cv2.contourArea(cnt)
-                    i=10
-                    if max_cos < 0.18:
-                        squares.append(cnt)
-                        roi = img0[y-i:y+h+i, x-i:x+w+i]
-                        code = decode(roi)
-                        return squares
 
 def data_matrix_demo(cap):
-    window_name = "datamatrix webcam scanner by llpassarelli"
+    window_name = "datamatrix scanner"
     frame_number = 0
     need_to_save = False
 
-    while 1:
+    while True:
+        e1 = cv2.getTickCount()
         ret, frame = cap.read()
         if not ret:
             break
+        imr = cv2.resize(
+            frame,
+            None,
+            fx=0.5,
+            fy=0.5,
+            interpolation=cv2.INTER_CUBIC)
+        try:
+            '''MEAN TRIGGER - DETECTA O OBJETO NO CENTRO DA FRAME'''
+            roi = imr[80:160, 80:240]
+            imdecode = imr.copy()
+            # imr[80:160, 80:240] = 255
+            means = cv2.mean(roi)
+            mean = str("%d" % means[0])
+            # print('mean:', mean)
+            cv2.rectangle(imr, (80, 80), (240, 160), (255, 255, 255), 1)
+            cv2.line(imr, (160, 70), (160, 170), (0, 0, 255), 1)
+            cv2.line(imr, (70, 120), (250, 120), (0, 0, 255), 1)
+            cv2.putText(imr, mean, (150, 70),
+                        cv2.FONT_HERSHEY_SIMPLEX, .4, (255, 0, 0), 1)
 
-        frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5) 
-        squares = find_squares(frame)
-        cv2.drawContours( frame, squares, -1, (0, 250, 0), 1 )
-        cv2.imshow(window_name, frame)
+            if means[0] > 100 and means[0] < 190:
+                cv2.rectangle(imr, (80, 80), (240, 160), (255, 255, 255), 1)
+                cv2.line(imr, (160, 80), (160, 160), (0, 255, 0), 1)
+                cv2.line(imr, (80, 120), (240, 120), (0, 255, 0), 1)
+                code, points = decode(imdecode)
+                for i, point in enumerate(points):
+                    cv2.line(imr, point, points[i - 1], (0, 255, 0), 1)
+                    cv2.putText(
+                        imr, code, points[0], cv2.FONT_HERSHEY_SIMPLEX, .4, (0, 250, 0), 1)
 
-        key = cv2.waitKey(30)
+        except Exception:
+            print "error decode"
+            pass
+
+        e2 = cv2.getTickCount()
+        t = (e2 - e1) / cv2.getTickFrequency()
+        fps = str("%.0f" % (1 / t))
+        msg = "fps: " + str(fps)
+        t = str("%.3f" % t)
+        msg = "time (s): " + str(t)
+        cv2.rectangle(imr, (0, 0), (90, 14), (0, 0, 0), -1)
+        cv2.putText(imr, t + 's ' + fps + 'fps', (1, 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, .4, (0, 250, 0), 1)
+        cv2.imshow(window_name, imr)
+        key = cv2.waitKey(1)
         c = chr(key & 255)
         if c in ['q', 'Q', chr(27)]:
             break
@@ -82,31 +91,43 @@ def data_matrix_demo(cap):
 
         frame_number += 1
 
+
 def decode(imgcv2):
     code = ""
     try:
+        st = datetime.datetime.now()
         img = Image.fromarray(imgcv2)
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
         dm_read = DataMatrix()
-        code = dm_read.decode(img.size[0], img.size[1], buffer(img.tostring()), timeout=120, maxcount=1, corrections=1)
-        date = datetime.datetime.now()
+        code = dm_read.decode(
+            img.size[0],
+            img.size[1],
+            buffer(
+                img.tostring()),
+            timeout=80,
+            maxcount=1,
+            corrections=3)
+        end = datetime.datetime.now()
         if dm_read.count() == 1:
-            print code, date
-            cv2.imshow(code, imgcv2)
+            # print code, end - st
+            time = end - st
+
+            print dm_read.stats(1), "t:", str(time)[6:]
+            points = dm_read.stats(1)[1]
+            # print "points:", points
+            # os.system("beep -f1296 -l10")
     except Exception:
+        print "error decode"
         pass
-    
-    return code
 
-    #if dm_read.count() == 1:
-       # os.system("beep -f1296 -l10")
-
-
+    return code, points
 
 if __name__ == '__main__':
     print __doc__
 
     if len(sys.argv) == 1:
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(1)
     else:
         cap = cv2.VideoCapture(sys.argv[1])
         if not cap.isOpened():
@@ -115,7 +136,4 @@ if __name__ == '__main__':
     if not cap.isOpened():
         print 'Cannot initialize video capture'
         sys.exit(-1)
-    #webcam delay
-    print "Inicializando"
-    time.sleep(3)
     data_matrix_demo(cap)
